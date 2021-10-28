e5a59560a22412e155860a1ced5e24f49cb08d54
Fernetazo <angelofdarkness88@gmail.com>
2021-10-28 16:21:12 -0300

    Render individual project

--- src/render.js
+++ src/render.js
@@ -10,74 +10,39 @@ const clearMainDisplay = () => {
 
 }
 
-const renderProject = () => {
+const renderProject = (e) => {
 
     clearMainDisplay();
+    
+    const targetProject = projects.find( (project) => project.title == e.target.textContent );
 
-    projects.forEach(e => {
+    let mainDisplay = document.querySelector('.mainDisplay');
 
-        let projectItem = document.createElement('div');
-        projectItem.classList.add('projectItem');
+    let projectItem = document.createElement('div');
+    let projectHeader = document.createElement('div');
+    let projectTitle = document.createElement('div');
+    let projectDescription = document.createElement('div');
+    let todos = document.createElement('div');
 
-        let projectHeader = document.createElement('div');
-        projectHeader.classList.add('projectHeader');
+    projectItem.classList.add('projectItem');
+    projectHeader.classList.add('projectHeader');
+    projectTitle.classList.add('projectTitle');
+    projectDescription.classList.add('projectDescription');
+    todos.classList.add('todos');
 
-        let projectTitle = document.createElement('div');
-        projectTitle.classList.add('title');
+    projectTitle.textContent = targetProject.title;
+    projectDescription.textContent = targetProject.description;
 
-        let projectDescription = document.createElement('div');
-        projectDescription.classList.add('description');
+    projectHeader.appendChild(projectTitle);
+    projectHeader.appendChild(projectDescription);
+    projectItem.appendChild(projectHeader);
+    projectItem.appendChild(todos);
 
-        let todoItem = document.createElement('div');
-        let leftSide = document.createElement('div');
-        let rightSide = document.createElement('div');
-        let priority = document.createElement('div');
-        let checkbox = document.createElement('input');
-        let label = document.createElement('label');
-        let dueDate = document.createElement('div');
-        let detailsButton = document.createElement('button');
-        let editButton = document.createElement('button');
-        let deleteButton = document.createElement('button');
+    mainDisplay.appendChild(projectItem);
 
-        todoItem.classList.add('todoItem');
-        leftSide.classList.add('todoLeftSide');
-        rightSide.classList.add('todoRightSide');
+    renderTODOList(targetProject.TODOlist);
         
-        priority.classList.add('priority');
-        priority.classList.add(e.priority);
-        priority.textContent = e.priority; //Delete this when style is added
-
-        checkbox.type = 'checkbox';
-        checkbox.classList.add('checkbox');
-        checkbox.textContent = e.checkbox;
-
-        label.for = 'todoExample';
-        label.textContent = e.title;
-
-        dueDate.classList.add('dueDate');
-        dueDate.textContent = e.dueDate;
-
-        detailsButton.classList.add('detailsButton');
-        detailsButton.textContent = 'Details';
-
-        editButton.classList.add('editButton');
-        editButton.textContent = 'Edit';
-
-        deleteButton.classList.add('deleteButton');
-        deleteButton.textContent = 'Delete';
-
-        leftSide.appendChild(priority);
-        leftSide.appendChild(checkbox);
-        leftSide.appendChild(label);
-        rightSide.appendChild(dueDate);
-        rightSide.appendChild(detailsButton);
-        rightSide.appendChild(editButton);
-        rightSide.appendChild(deleteButton);
-        todoItem.appendChild(leftSide);
-        todoItem.appendChild(rightSide);
-        mainList.appendChild(todoItem);
-    });
-}
+};
 
 function render() {
 
@@ -91,14 +56,15 @@ function render() {
         projectItems.appendChild(projectItem);
     });
 
-    renderMainList();
+    renderTODOList(mainTODOlist);
 }
 
-const renderMainList = () => {
+const renderTODOList = (target) => {
+
 
     let mainList = document.querySelector('.todos');
-
-    mainTODOlist.forEach(e => {
+    
+    target.forEach(e => {
 
         let todoItem = document.createElement('div');
         let leftSide = document.createElement('div');
