d9e35bdf074231e53f238e65c7c2da939e6a0b24
Fernetazo <angelofdarkness88@gmail.com>
2021-11-30 16:39:56 -0300

    [UI] Modal styling

--- src/index.js
+++ src/index.js
@@ -32,9 +32,12 @@ if (!mainTODOlist) {
 
     mainTODOlist = [];
 
-    mainTODOlist.push(new singleTODO('High', true,'Do the dishes', date,'Remember to use the sponge!'));
-    mainTODOlist.push(new singleTODO('Medium', false,'Wash the car', date));
-    mainTODOlist.push(new singleTODO('Low', true,'Pet the cat', date,'Miau'));
+    mainTODOlist.push(new singleTODO('High', false,'High priority task example', date,'Remember to be happy! :)'));
+    mainTODOlist.push(new singleTODO('Medium', false,'Medium priority task example', date));
+    mainTODOlist.push(new singleTODO('Low', false,'Low priority task example', date,'Details example'));
+    mainTODOlist.push(new singleTODO('Low', true,'Done task example 1', date,'Details example'));
+    mainTODOlist.push(new singleTODO('Medium', true,'Done task example 2', date,'Details example'));
+    mainTODOlist.push(new singleTODO('High', true,'To do and done tasks stay sorted!', date,'Details example'));
 
 }
 
@@ -42,14 +45,15 @@ if (!projects) {
 
     projects = [];
 
-    projects.push(new Project('Vacation', 'Going to Rome!'));
-    projects.push(new Project('Buy new car', 'I like the white one'));
+    projects.push(new Project('Project example', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
+    projects[0].TODOlist.push(new singleTODO('Medium', false,'Buy something', date,'Should buy on Mercado Libre'));
+    projects[0].TODOlist.push(new singleTODO('High', false,'Check something', date,'Check on Google Flights'));
+    projects[0].TODOlist.push(new singleTODO('Low', true,'Done thing 1', date,'Yup!'));
+    projects[0].TODOlist.push(new singleTODO('Low', true,'Done thing 2', date,'Wacka!'));
 
-    projects[0].TODOlist.push(new singleTODO('Medium', true,'Buy tickets', date,'Should buy on despegar.com'));
-    projects[0].TODOlist.push(new singleTODO('High', true,'Check prices]', date,'Check on google flights'));
-
+    projects.push(new Project('Buy new car', 'The red one will be mine!'));
     projects[1].TODOlist.push(new singleTODO('High', true,'Check model T', date,'I really want a Tesla'));
-    projects[1].TODOlist.push(new singleTODO('Low', true,'Buy the goddam car', date,'Oh yeah!'));
+    projects[1].TODOlist.push(new singleTODO('Low', false,'Buy the car', date,'Oh yeah!'));
 
 }
 
